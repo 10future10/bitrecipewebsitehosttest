3dedfb4632a0480e77fe2f54a51911bab8ef5243
Lua_Python <137715789+10future10@users.noreply.github.com>
2023-10-21 17:54:26 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -2,8 +2,30 @@ function recipename() {
     document.getElementById('box1').innerHTML = "Hi";
 }
 
+var recipenametitlemain = document.getElementById('titlerecipemain').innerHTML
+
 var visible_enter_btn = 0;
 
+function showmeals() {
+    var meals = document.getElementById('meals');
+    meals.style.display = 'block';
+}
+
+function showpepporoniepizzameal() {
+    var pizzapepporniemeal = document.getElementById('pepperoniepizzamain');
+    var cheeseburgermeal = document.getElementById('cheeseburgeramain');
+
+    cheeseburgermeal.style.display = 'none';
+    pizzapepporniemeal.style.display = 'block';
+}
+
+function showcheeseburgermeal() {
+    var cheeseburgermeal = document.getElementById('cheeseburgeramain');
+    var pizzapepporniemeal = document.getElementById('pepperoniepizzamain');
+
+    cheeseburgermeal.style.display = 'block';
+    pizzapepporniemeal.style.display = 'none';
+}
 function started() {
     var div1 = document.getElementById('main');
     var div2 = document.getElementById('intro');
